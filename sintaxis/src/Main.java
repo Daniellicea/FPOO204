cc924f7d2b59a08a9735c45ae137d4b46c399fc7
Daniellicea <123047951@upq.edu.mx>
2025-01-21 17:16:12 -0600

    Se cambio el hola mundo por Eduardo Daniel

--- sintaxis/src/Main.java
+++ sintaxis/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello world");
 
-        System.out.printf("Hola Eduardo Daniel");
+
+        
+        System.out.printf("Hola Eduardo Daniel Licea");
     }
 }
