39d2670ad544e0a24d498276e3c65457890a267c
Daniellicea <123047951@upq.edu.mx>
2025-01-22 22:05:14 -0600

    Estructura de java

--- sintaxis/src/Main.java
+++ sintaxis/src/Main.java
@@ -1,10 +1,26 @@
+import java.util.*;
+import java.beans.JavaBean;
 public class Main {
 
     public static void main(String[] args) {
         System.out.printf("Hello world");
 
 
-        
+
         System.out.printf("Hola Eduardo Daniel Licea");
+
+        /*
+
+        soy
+        un
+        comentario
+        de
+        varias
+        linas
+
+         */
+
+        System.out.printf("Licea Gonzalez Eduardo Daniel");
+        String Cadenas = "Eduardo" + "dANIEL "+ "Licea" + "Gonzalez";
     }
 }
