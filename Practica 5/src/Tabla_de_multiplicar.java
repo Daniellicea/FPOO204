0931b8286306f9c2954eb7e0f3ecceff81c19b49
Daniellicea <123047951@upq.edu.mx>
2025-01-29 15:02:16 -0600

    Tablas de multiplicar

---
+++ Practica 5/src/Tabla_de_multiplicar.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Tabla_de_multiplicar {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(1+"x"+i+"="+(1*i));
+        }
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(2+"x"+i+"="+(2*i));
+        }
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(3+"x"+i+"="+(3*i));
+        }
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(4+"x"+i+"="+(4*i));
+        }
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(5+"x"+i+"="+(5*i));
+        }
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(6+"x"+i+"="+(6*i));
+        }
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(7+"x"+i+"="+(7*i));
+        }
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(8+"x"+i+"="+(8*i));
+        }
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(9+"x"+i+"="+(9*i));
+        }
+        for (int i = 1; i <= 10; i ++) {
+            System.out.println(10+"x"+i+"="+(10*i));
+        }
+    }
+}
+
+
