b4b4cd4f26e5390ecd1a92dc85eb4034d2fa71f0
Daniellicea <123047951@upq.edu.mx>
2025-01-29 15:14:05 -0600

    numero impar

---
+++ Practica 5/src/Numero_positivo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Numero_positivo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número positivo: ");
+        int numero = scanner.nextInt();
+
+        if (numero <= 0) {
+            System.out.println("Por favor, ingrese un número positivo.");
+        } else {
+            System.out.print("Números impares desde 1 hasta " + numero + ": ");
+            for (int i = 1; i <= numero; i += 2) {
+                if (i > 1) {
+                    System.out.print(", ");
+                }
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
