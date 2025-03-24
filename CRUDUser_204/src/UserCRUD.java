ecd247f250fcd609fd18f5885b59f58282897d7c
Daniellicea <123047951@upq.edu.mx>
2025-03-24 15:50:57 -0600

    Metodos y verificacione de errores

--- CRUDUser_204/src/UserCRUD.java
+++ CRUDUser_204/src/UserCRUD.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
+import java.sql.ResultSet;
 
 
 public class UserCRUD {
@@ -25,6 +25,19 @@ public class UserCRUD {
             return false;
         }
     }
+    
+    public ResultSet Buscar_por_id (int id){
+        String sq_buscar_id = "Select * from usuarios where id_usuario1 = ? ";
+        try{
+            PreparedStatement ps = conexion.prepareStatement(sq_buscar_id);
+            ps.setInt(1, id);
+            return ps.executeQuery();
+        }catch (SQLException e){
+            System.out.println("Error al buscar por Idi"+e.getMessage());
+            return null;
+        }
+    }
+    
     
     
 }
